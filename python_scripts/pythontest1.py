4122ab055f2b6690ddae9ad8b5775c8cbb453305
eddy913 <eddy913@gmail.com>
2017-10-17 12:22:10 -0400

    add files

---
+++ python_scripts/pythontest1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+name = "xuan"
+color = "blue"
+animal = "cat"
+print (name, "loves", color, "and", animal)
+print ("done")
---
+++ python_scripts/pythontest2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+a =str(sys.argv[1])
+b = str(sys.argv[2])
+c = str(sys.argv[3])
+print(a,"loves",b,"and",c)
+print ("done")
